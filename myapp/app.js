65403f0ef7b57528f472c0d7aba15b41e64e2b58
arnmontez <arnaud.montez@gmail.com>
2018-11-07 10:07:09 +0100

    express_generator exo4 passport

--- myapp/app.js
+++ myapp/app.js
@@ -4,49 +4,48 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
-var getdataRouter = require('./routes/getdata');
-var trucRouter = require('./routes/truc');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+/* var auth = require('Auth'); */
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    console.log("testes");
+    if(username=="a" && password=="aa") return true;
+    else console.log("erreur");   
+}));
 
 
 var app = express();
 
-// view engine setup
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',
+    function(req,res){
+        res.render('home', { user: req.user });
+    });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/test', testRouter);
-app.use('/getdata',getdataRouter);
-app.use('/gettruc',trucRouter);
+app.get('/login',
+    function(req,res){
+        res.render('login');
+    });
 
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    function(req,res){
+        res.redirect('/');
+    });
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/logout',
+    function(req, res){
+    req.logout();
+    res.redirect('/');   
+    });
 
 module.exports = app;
---
+++ myapp/auth/user.js
@@ -0,0 +1,4 @@
+var records = [
+{ id: 1, username: 'jack', password : 'sec', displayname: 'jac', emails: [ { value: 'tt@gmail.com'}, { value: 'tt2@gmail.com'} ]},
+{ id: 2, username: 'jick', password : 'sec', displayname: 'jic', emails: [ { value: 'tt3@gmail.com'}, { value: 'tt4@gmail.com'} ]},
+];
